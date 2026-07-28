57f5fab3f132c6d06d943d38441d026a1fdd61b7
sravanijalibili <48159358+sravanijalibili@users.noreply.github.com>
2026-07-28 18:43:42 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "https://fresh-backend-1007.onrender.com/api",
+/",
 });
 
 export default API;
