db578d7d9c4a760e6b9f392e1488e2fdcc619ae4
sravanijalibili <48159358+sravanijalibili@users.noreply.github.com>
2026-08-14 17:57:48 +0530

    Update AddressForm.jsx

--- frontend/src/components/AddressForm.jsx
+++ frontend/src/components/AddressForm.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import L from "leaflet";
 import toast from "react-hot-toast";
 import { createAddress, updateAddress } from "../services/addressService";
-import "../styles/address-form.css";
+import "../styles/addressForm.css";
 import "leaflet/dist/leaflet.css";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
